07a133c0b39ea3e82e3abfa4774689473e96c2dc
KaushikDebdas <kaushikdebdas27@gami.com>
2021-12-09 00:22:10 +0600

    component,inline css,object css

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Heading</h1>
+        <p>{2+3}</p>
+        <p>My First React Paragraph</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  // Object CSS
+  const personStyle = {
+    border:'2px solid red',
+    margin: '10px'
+  }
+  return (
+  <div style={personStyle}>
+    <h1>Name: Sakib Khan</h1>
+    <h3 style={{ color:'yellowgreen' }}>Hero of the year</h3>
+  </div>
+  )
 }
 
 export default App;
